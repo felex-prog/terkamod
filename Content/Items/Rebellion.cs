4a18d8b1d954391b23518454520bfd47147c4d57
sparda1233 <jeffersonconnor3@gmail.com>
2025-06-20 16:24:14 +0300

    Создан мятежник

---
+++ Content/Items/Rebellion.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TurtleMod.Content.Items
+{
+    // This is a basic item template.
+    // Please see tModLoader's ExampleMod for every other example:
+    // https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+    public class Rebellion : ModItem
+    {
+        // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TurtleMod.hjson' file.
+        public override void SetDefaults()
+        {
+            Item.damage = 120;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 18;
+            Item.useAnimation = 18;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4;
+            Item.value = Item.buyPrice(gold: 5);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.SilverBar, 20);
+            recipe.AddIngredient(ItemID.IronBar, 30);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
