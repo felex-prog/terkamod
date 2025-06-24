51af70d1a42a8cebb8a81e232adf48f7295ce775
asics3232 <alecseivic01@gmail.com>
2025-06-24 12:50:38 +0300

    Добавлена способность меча

---
+++ Content/Player/BloodShadowAbilityPlayer.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameInput;
+using Microsoft.Xna.Framework;
+using TurtleMod.Systems;
+using TurtleMod.Content.Projectiles;
+
+
+namespace TurtleMod.Players
+{
+    public class BloodShadowAbilityPLayer : ModPlayer
+    {
+        public override void ProcessTriggers(TriggersSet triggersSet)
+        {
+            var KeyBindSystem= ModContent.GetInstance<KeyBindSystem>();
+            if (KeyBindSystem.BloodShadowAbility.JustPressed)
+            {
+                var shadowPlayer = Player.GetModPlayer<BloodShadowPlayer>();
+                if (!shadowPlayer.shadowActivate && shadowPlayer.CooldownTimer <= 0)
+                {
+                    shadowPlayer.shadowActivate = true;
+                    shadowPlayer.AbilityTimer = 1200;
+                    shadowPlayer.CooldownTimer = 3600;
+
+                    Projectile.NewProjectile(Player.GetSource_FromThis(), Player.Center, Vector2.Zero, ModContent.ProjectileType<BloodShadow>(), 0, 0, Player.whoAmI);
+                }
+            }
+        }
+    }
+    
+}
---
+++ Content/Player/BloodShadowPlayer.cs
@@ -0,0 +1,58 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using TurtleMod.Content.Projectiles;
+using Terraria.DataStructures;
+using TurtleMod.Content.Items;
+using Microsoft.Xna.Framework;
+
+
+namespace TurtleMod.Players
+{
+    public class BloodShadowPlayer : ModPlayer
+    {
+        public bool shadowActivate { get; set; }
+        public int AbilityTimer { get; set; }
+        public int CooldownTimer { get; set; }
+
+        public const int MaxCooldown = 3600;
+        public const int MaxAbilityDuration = 1200;
+
+        public void ActivateAbility()
+        {
+            shadowActivate = true;
+            AbilityTimer = MaxAbilityDuration;
+            CooldownTimer = MaxCooldown;
+
+            if (Player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(Player.GetSource_FromThis(), Player.Center, Vector2.Zero, ModContent.ProjectileType<BloodShadow>(), 0, 0, Player.whoAmI);
+            }
+        }
+        public override void ResetEffects()
+        {
+           
+            {
+                Player.immune = true;
+                Player.immuneTime = 2;
+                Player.invis = true;
+
+                if (--AbilityTimer <= 0)
+                 shadowActivate = false;
+
+                if (CooldownTimer > 0)
+                    CooldownTimer--;
+            }
+          
+
+            
+        }
+        public override void ModifyHitNPCWithItem(Item item, NPC target, ref NPC.HitModifiers modifiers)
+        {
+            if (shadowActivate && item.type == ModContent.ItemType<FleshSword>())
+            {
+                modifiers.SetCrit();
+                modifiers.CritDamage += 2f;
+            }
+        }
+    }
+}
