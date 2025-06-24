c44149c9b1ae5fb0d312b7b75e22804495ca4c2a
asics3232 <alecseivic01@gmail.com>
2025-06-24 12:50:56 +0300

    Добавлена система бинда кнопок

---
+++ Content/Systems/KeyBindSystem.cs
@@ -0,0 +1,19 @@
+﻿using Terraria.ModLoader;    
+
+namespace TurtleMod.Systems
+{
+    public class KeyBindSystem : ModSystem
+    {
+        public static ModKeybind BloodShadowAbility { get; private set; }
+
+        public override void Load()
+        {
+            BloodShadowAbility = KeybindLoader.RegisterKeybind(Mod, "Activation Blood Shadow", "G");
+        }
+
+        public override void Unload()
+        {
+            BloodShadowAbility = null;
+        }
+    }
+}
